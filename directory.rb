96d47c8427face53cb5d7b2be1b2f6b1b6ba3ca5
toby <toby.hw@live.co.uk>
2017-04-28 19:45:54 +0100

    Added save function and .gitignore for file students.csv

--- directory.rb
+++ directory.rb
@@ -15,6 +15,19 @@ def input_students
   end
 end
 
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+  puts "Student list saved"
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "------------"
@@ -33,6 +46,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the lists to students.csv"
   puts "9. Exit" #9 because we'll add more items inbetween
 end
 
@@ -48,6 +62,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
