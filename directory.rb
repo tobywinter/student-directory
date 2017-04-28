bb9254a0c923e2177806f185871fc0f3bca308c7
toby <toby.hw@live.co.uk>
2017-04-28 15:03:48 +0100

    Refactored long methods, using separate methods, improved readability

--- directory.rb
+++ directory.rb
@@ -13,8 +13,6 @@ def input_students
     #get another name from the user
     name = gets.chomp
   end
-  #return the array of students
-  @students
 end
 
 def print_header
@@ -22,7 +20,7 @@ def print_header
   puts "------------"
 end
 
-def print(students)
+def print_students_list
   @students.each do |student|
     puts "#{@students.find_index(student)+1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
@@ -40,27 +38,27 @@ end
 
 def show_students
   print_header
-  print(@students)
+  print_students_list
   print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+  end
 end
 
 def interactive_menu
   loop do
-    # 1. print the menu and ask the user what to do
     print_menu
-  # 2. read the input and save it into a variable
-    selection = gets.chomp
-  # 3. do what the user has asked
-    case selection
-    when "1"
-        students = input_students
-      when "2"
-        show_students
-      when "9"
-        exit #this will cause the program to terminate
-      else
-        puts "I don't know what you meant, try again"
-    end
+    process(gets.chomp)
   end
 end
 
