5d85c1e41478ba5c8b542f57a62459f2e36e9924
toby <toby.hw@live.co.uk>
2017-04-28 01:04:40 +0100

    Added block to iterate and print student array

--- directory.rb
+++ directory.rb
@@ -1,4 +1,3 @@
-student_count = 11
 #first we print the list of students
 puts "The students of Villains Academy"
 students = [
@@ -14,18 +13,10 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates"
 ]
+# and then print them
 puts "------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
-puts students[11]
+students.each do |student|
+  puts student
+end
 #finally, we print the total number of students
-print "Overall, we have #{student_count} great students"
+print "Overall, we have #{students.count} great students"
