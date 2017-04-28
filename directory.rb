8214f60964cae19ca4a80b9559c00bcf64973b0a
toby <toby.hw@live.co.uk>
2017-04-28 14:38:52 +0100

    Made students an instance variable @students

--- directory.rb
+++ directory.rb
@@ -1,20 +1,20 @@
+@students = [] #an empty array accessibly to all methods (instance variable?)
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #create and empty array
-  students = []
   #get the first name
   name = gets.chomp
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :November}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :November}
+    puts "Now we have #{@students.count} students"
     #get another name from the user
     name = gets.chomp
   end
   #return the array of students
-  students
+  @students
 end
 
 def print_header
@@ -23,32 +23,39 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{students.find_index(student)+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  @students.each do |student|
+    puts "#{@students.find_index(student)+1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" #9 because we'll add more items inbetween
+end
+
+def show_students
+  print_header
+  print(@students)
+  print_footer
 end
 
 def interactive_menu
-  students = []
   loop do
     # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" #9 because we'll add more items inbetween
+    print_menu
   # 2. read the input and save it into a variable
     selection = gets.chomp
   # 3. do what the user has asked
     case selection
-      when "1"
+    when "1"
         students = input_students
       when "2"
-        print_header
-        print(students)
-        print_footer(students)
+        show_students
       when "9"
         exit #this will cause the program to terminate
       else
